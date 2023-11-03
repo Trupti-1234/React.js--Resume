336e3ef1cf271b997257be544515f20a050a8254
trupti <truptia081@gmail.com>
2023-11-03 10:48:19 +0530

    attempt

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Resume from './Component/Resume/Resume';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+     <Resume/>
+       
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Component/Resume/Resume.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import image from './img.jpg'
+
+function Resume() {
+    return (
+        <div class="col"><h1 class="main">Resume</h1>
+            <div class="line"></div>
+            <img src='image' alt=''></img>
+            <div>NAME:Trupti R Acharya</div>
+            <div>EMAIL:truptia.081@gmail.com</div>
+            <div>PHONE:1122334455</div>
+           
+        
+            
+            < div class=" info1">
+                <div class="ab"><h2><u>About</u></h2></div>
+                <div class='font'>I'm a Third year computer science engineering student.
+                    Passionate in learning new things in the field of engineering.Hardworking Student seeking employment.
+                     Ready to utilize my skills and passion to further the mission of a company.
+                    Technologically adept, a positive attitude, willingness and motivation to learn new programs. 
+                </div>
+                <div class="ed"><h2>Education</h2></div>
+                <div class="edu">School : Little Rock Indian School [2019 batch] 
+                    <br></br>Percentage:83%<br></br>College : Vidyodaya Pre-University College[2021 batch] <br></br>Percentage:86%
+                </div>
+                <div class="ab"><h2><u>Technical Skills</u></h2></div>
+                <div class='font'><li>C</li> <li>C++</li><li>Python</li><li>Java</li><li>HTML</li>
+                     
+                </div>
+                <div class="ab"><h2><u>Achievements</u></h2></div>
+                <div class ="edu">Participated in the mini project conducted by the college.<br></br>Recived a certificate in CyberSecurity and Ethical Hacking
+                <br></br>Project on Stock-data Visualisation<br></br>Participated in the Intra-Intership conducated by the college</div>
+
+            </div>
+            
+
+
+            </div>
+        
+    )
+}
+
+export default Resume
